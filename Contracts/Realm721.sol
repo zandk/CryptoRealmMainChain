997966cce1a665c5ea377bdef0cfc90511ecffde
koday1 <34075192+koday1@users.noreply.github.com>
2018-06-28 15:56:24 -0600

    Added randomResource function & ERC721 functions

--- Contracts/Realm721.sol
+++ Contracts/Realm721.sol
@@ -3,13 +3,53 @@ pragma solidity ^0.4.4;
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 import "openzeppelin-solidity/contracts/Math/SafeMath.sol";
 
-contract Realm721 is ERC721{
+contract Realm721 is TileCreation, ERC721{
 
-    //@dev Using safemath
-    using SafeMath for uint 256;
+    //Using safemath
+    using SafeMath for uint256;
 
-    //TO-DO @dev ownership modifier
+    //Ownership modifier (how can I access 'tileId'?)
+    modifier onlyTileOwner(uint tileId){
+        require(msg.sender == tileIdToOwner[tileId]);
+        _;
+    }
 
-    ....
+    //Mapping for tile approvals
+    mapping(uint => address) tileApprovals;
 
+    //Approval function
+    function approve(address _to, uint256 _tokenId) public onlyTileOwner(tileId) {
+        tileApprovals[_tokenId] = _to;
+        Approval(msg.sender, _to, _tokenId);
+    }
+
+    //Token transfer logic 
+    function _transfer(address _from, address _to, uint256 _tokenId) private {
+        ownershipCount[_to] = ownershipCount[_to].add(1);
+        ownershipCount[msg.sender] = ownershipCount[msg.sender].sub(1);
+        tileIdToOwner[_tokenId] = _to;
+        Transfer(_from, _to, _tokenId);
+    }
+
+    //Transfer function (only owner)
+    function transfer(address _to, uint256 _tokenId) public onlyTileOwner(tileId) {
+        _transfer(msg.sender, _to, _tokenId);
+    }
+
+    //Returns balance for a specific account
+    function balanceOf(address _owner) public view returns (uint256) {
+        return ownershipCount[_owner];
+    }
+
+    //Returns owner of a given token ID
+    function ownerOf(uint256 _tokenId) public view returns (address _owner) {
+        return tileIdToOwner[_tokenId];
+    }
+
+    //Transfer function (approval method)
+    function takeOwnership(uint256 _tokenId) public {
+        require(tileApprovals[_tokenId] == msg.sender);
+        address owner = ownerOf(_tokenId);
+        _transfer(owner, msg.sender, _tokenId);
+    }
 }
--- Contracts/TileCreation.sol
+++ Contracts/TileCreation.sol
@@ -4,5 +4,125 @@ import "openzeppelin-solidity/contracts/Math/SafeMath.sol";
 
 contract TileCreation{
 
-    ....
+    //Using safemath
+    using SafeMath for uint256;
+    //Events
+    event UpdatedTile(uint tileId);
+    
+    // Tile struct
+    struct Tile {
+        int8 x;
+        int8 y;
+    }
+
+    int8 ResourceType;
+    int8 ResourceAmount;
+
+    // -- Storage --
+    // World
+    Tile[] tiles;
+    mapping (int => mapping(int => uint)) positionToTileId;
+    mapping (uint => address) tileIdToOwner;
+    mapping (address => uint) ownershipCount;
+
+    function createTile(int8 _x, int8 _y) internal {
+        // TODO - make sure position isn't already occupied
+        //   and that it is valid (next to another tile)
+        uint id = tiles.push(Tile(_x, _y)) - 1;
+        positionToTileId[_x][_y] = id;
+    }
+    
+    uint randNonce = 0;
+
+    //Function to create psuedo-random uints for resource type and amount
+    function randomResources(int8 Xpos, int8 Ypos) internal {
+        ResourceType = int8(keccak256(now, randNonce, Xpos, Ypos)) % 100;
+        randNonce++;
+        ResourceAmount = int8(keccak256(now, randNonce, Xpos, Ypos)) % 100;
+    }
+
+    function RealmBase() public {
+        // Create base tiles with resources
+        //(should this have an onlyOwner modifier?)
+
+        // Genesis tile! id = 0
+        createTile(0, 0);
+        randomResources(0,0);
+
+        createTile(0, -1);
+        randomResources(0,-1);
+
+        createTile(0, 1);
+        randomResources(0,1);
+
+        createTile(-1, 0);
+        randomResources(-1,0);
+
+        createTile(1, 0);
+        randomResources(1,0);
+
+        createTile(1, -1);
+        randomResources(1,-1);
+
+        createTile(-1, 1);
+        randomResources(-1,1);
+
+    }
+
+    function tileExists(int8 _x, int8 _y) internal view returns(bool) {
+        if ( (_x != 0 || _y != 0) && (positionToTileId[_x][_y] == 0)) {
+            return false;
+        }
+        return true;
+    }
+    
+    /** @dev function for creating a new "guy"
+      * @return worked Whether or not claim went through
+      */
+    function ClaimTile(uint _id) public returns(bool) {
+        require(tileIdToOwner[_id] == address(0));
+
+        // If we are claiming a tile for the first time
+        Tile t = tiles[_id];
+        if (!tileExists(t.x+1, t.y)) {
+            createTile(t.x+1, t.y);
+        }
+        if (!tileExists(t.x, t.y+1)) {
+            createTile(t.x, t.y+1);
+        }
+        if (!tileExists(t.x-1, t.y)) {
+            createTile(t.x-1, t.y);
+        }
+        if (!tileExists(t.x, t.y-1)) {
+            createTile(t.x, t.y-1);
+        }
+        if (!tileExists(t.x+1, t.y-1)) {
+            createTile(t.x+1, t.y-1);
+        }
+        if (!tileExists(t.x-1, t.y+1)) {
+            createTile(t.x-1, t.y+1);
+        }
+
+        tileIdToOwner[_id] = msg.sender;
+        emit UpdatedTile(_id);
+        // emit NewGuy(id, _name, _dna);
+    }
+
+    /** @dev Returns the amount of tiles in the world
+      * @return length The length of tiles in the world.
+      */
+    function GetTileCount() public view returns(uint) {
+        return tiles.length;
+    }
+
+    /** @dev Gets data for a specific tile in the world
+      * @param _id id of the tile to get data for.
+      * @return owner The address of the owner of this tile.
+      * @return x The x position of this tile.
+      * @return y The y position of this tile.
+      */
+    function GetTile(uint _id) public view returns(address, int8, int8) {
+        return (tileIdToOwner[_id], tiles[_id].x, tiles[_id].y);
+    }
+
 }
