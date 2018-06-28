4227451f9a1f77f6f8fec867586021f476e146d8
koday1 <34075192+koday1@users.noreply.github.com>
2018-06-27 20:18:54 -0600

    Creating initial contract files

---
+++ Realm721.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.4;
+
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-solidity/contracts/Math/SafeMath.sol";
+
+contract Realm721 is ERC721{
+
+    //@dev Using safemath
+    using SafeMath for uint 256;
+
+    //TO-DO @dev ownership modifier
+
+    ....
+
+}
---
+++ TileCreation.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.4;
+
+import "openzeppelin-solidity/contracts/Math/SafeMath.sol";
+
+contract TileCreation{
+
+    ....
+}
